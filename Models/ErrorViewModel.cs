a51ca9252b766da7fd36794794902991fd8db659
Chris Wong <wc38.wong@gmail.com>
2021-07-24 20:42:40 +0800

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace url_referrer_in_asp_net_core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
